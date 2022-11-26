658e3b2f37bfaf3be30e403c9f207114421d7d76
LuisKlopp <fbgus333@gmail.com>
2022-11-26 23:30:07 +0900

    feat: 커밋용테스트

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline m-10">Hello world!</h1>
+      <div className="flex w-150 h-100">
+        <h1 className="text-3xl font-bold underline m-10">test</h1>
+        <div className="w-150 h-20 bg-black flex justify-center items-center">
+          <h1 className="text-xl text-white">테스트..</h1>
+        </div>
+      </div>
     </div>
   );
 }
