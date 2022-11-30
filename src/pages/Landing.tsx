a1b6592784888c327e11ff32bba1a62ae52e7d0e
LuisKlopp <fbgus333@gmail.com>
2022-11-30 14:51:37 +0900

    feat:폴더구조수정

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Landing = () => {
+  return <div>라우팅테스트</div>;
+};
+
+export default Landing;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FooterBar from "../components/FooterBar";
+import Landing from "../pages/Landing";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route />
+        <Route path="/" element={<Landing />} />
       </Routes>
       <FooterBar />
     </BrowserRouter>
