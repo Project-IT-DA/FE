6b33898ac67766182fdb4eaee940f40acdca261e
G-Hae <sifialee@gmail.com>
2022-12-01 00:01:11 +0900

    fix: 헤더 중복 겹치는 문제 해결 - #6

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -3,11 +3,7 @@ import itdatext from "../assets/main/itdatext.png";
 
 const LandingHeader = () => {
   return (
-    <div className="border-b w-full h-1/3 border-solid border-black">
-      <div className="w-1/6 m-1/5 absolute left-3 top-3">
-        <img src={itdatext} className="w-4/5 h-8"></img>
-      </div>
-    </div>
+    <div className="border-b w-full h-1/3 border-solid border-black"></div>
   );
 };
 
