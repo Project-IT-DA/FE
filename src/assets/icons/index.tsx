b8eeb8ba09204c54dd64935eb4152f40283173ce
Jinseo Noh <90177259+bennoholik@users.noreply.github.com>
2022-11-30 15:14:16 +0900

    [feature]footer 아이콘 추가

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,4 @@
+import { ReactComponent as HomeIcon } from "./HomeIcon.svg";
+import { ReactComponent as ChatIcon } from "./ChatIcon.svg";
+import { ReactComponent as CommunityIcon } from "./CommunityIcon.svg";
+export { HomeIcon, ChatIcon, CommunityIcon };
