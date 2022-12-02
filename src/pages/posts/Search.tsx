ec431b4b15e4dc27480e12573700a6694e4f1dbe
hyolimLim <hyolimlim.dev@gmail.com>
2022-12-02 23:51:56 +0900

    feat: 카테고리 버튼 클릭 이벤트

--- src/pages/posts/Search.tsx
+++ src/pages/posts/Search.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { FilterIcon, ArrowUpIcon } from "../../assets/icons";
+import { useState } from "react";
+import { ArrowUpIcon } from "../../assets/icons";
 
 function Search() {
-  //1.클릭하면 아이콘이 사라지고, 키워드 부분이 늘어남.
-  //2.하단에 뭐가 생김. 그리고 배경 회색.
+  const [isClicked, setIsClicked] = useState(false);
+
+  const showCategory = () => {
+    setIsClicked(!isClicked);
+  };
 
   return (
     <div className="w-full mt-[20px]">
@@ -12,14 +15,21 @@ function Search() {
           className="w-full h-[44px] px-4 bg-[#EAEAEA] rounded-lg"
           placeholder="상품 키워드 검색 (12자 이내)"
         />
-        {/* <FilterIcon className="w-[44px] h-[44px] ml-1" cursor="pointer" /> */}
       </div>
-      <div className="w-full h-28 drop-shadow-2xl">
+      <div className="w-full h-fit">
         <div className="flex justify-between py-3">
           <p className="font-bold">카테고리</p>
-          <ArrowUpIcon className="w-4 h-4" />
+          <ArrowUpIcon
+            className="w-4 h-4"
+            onClick={showCategory}
+            cursor="pointer"
+          />
         </div>
-        <div className="grid grid-cols-3 grid-rows-2 gap-y-1.5 px-3 text-center">
+        <div
+          className={`${
+            !isClicked ? "hidden" : "grid"
+          } grid-cols-3 grid-rows-2 gap-y-1.5 px-3 mb-3 text-center`}
+        >
           <p>컴퓨터/노트북</p>
           <p>태블릿/핸드폰</p>
           <p>주변 기기</p>
