45ef1ba2ae4c5b2e30a0d3cf4413b5998a9159e7
LuisKlopp <fbgus333@gmail.com>
2022-12-06 01:47:21 +0900

    feat: 모바일 터치 드래그 완료

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -23,7 +23,6 @@ export function Carousel(props: {
     setIsClick(true);
     setMouseDownClientX(e.pageX);
   };
-
   const onMouseUp = () => {
     setIsClick(false);
     const imgX = mouseDownClientX - mouseUpClientX;
@@ -34,11 +33,22 @@ export function Carousel(props: {
       setNum(num => num + 1);
     }
   };
-
   const onMouseMove = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     if (!isClick) return;
     e.preventDefault();
     setMouseUpClientX(e.pageX);
+  };
+  const onTouchStart = (e: React.TouchEvent) => {
+    setIsClick(true);
+    setMouseDownClientX(e.changedTouches[0].pageX);
+  };
+  const onTouchEnd = (e: React.TouchEvent) => {
+    const distanceX = mouseDownClientX - e.changedTouches[0].pageX;
+    if (distanceX < -50 && num > 0) {
+      setNum(num => num - 1);
+    } else if (distanceX > 50 && num < 2) {
+      setNum(num => num + 1);
+    }
   };
 
   return (
@@ -49,6 +59,8 @@ export function Carousel(props: {
         onMouseDown={onMouseDown}
         onMouseUp={onMouseUp}
         onMouseMove={onMouseMove}
+        onTouchStart={onTouchStart}
+        onTouchEnd={onTouchEnd}
       >
         {images.map(image => {
           return (
