16af95fb004108cfe8a003285056de3c4c2edfbe
LuisKlopp <fbgus333@gmail.com>
2022-12-07 00:18:55 +0900

    feat: 랜딩 폰트추가

--- src/pages/landing/LandingHeader.tsx
+++ src/pages/landing/LandingHeader.tsx
@@ -4,7 +4,7 @@ const LandingHeader = ({ num, setNum }: Inum) => {
   const dotArray = [0, 1, 2];
   return (
     <div className="absolute z-10 w-full h-[35%] border-solid border-black flex justify-center flex-col items-center">
-      <div className="text-[1.2em]">
+      <div className=" font-bold text-xl text-[#393939] tracking-wide">
         {num === 0
           ? "IT에 관한 모든 중고거래 플랫폼"
           : num === 1
@@ -14,8 +14,8 @@ const LandingHeader = ({ num, setNum }: Inum) => {
           : null}
       </div>
       <div className="flex m-5 h-[25%] w-[100%] justify-center">
-        <span className=" text-[3em] font-bold">잇다는</span>
-        <span className="text-[3em] ml-5 text-[#ED2A70] font-bold">
+        <span className=" text-[3em] font-itda tracking-normal">잇다는</span>
+        <span className="text-[3em] ml-5 text-[#ED2A70] font-itda tracking-wide">
           다-있다
         </span>
       </div>
